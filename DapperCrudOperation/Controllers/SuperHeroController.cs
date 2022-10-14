18a7d8c853a2db08fd5dc2d51c4ec2b510d324b9
Shyam Agarwal <shyam@sumedhasoftech.com>
2022-10-14 16:15:48 +0530

    First

---
+++ DapperCrudOperation/Controllers/SuperHeroController.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+//using System.Data.SqlClient;
+
+namespace DapperCrudOperation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public SuperHeroController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpGet]
+        public async Task<ActionResult<SuperHero>> GetAllSuperHeros()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            //IEnumerable<SuperHero> heros = await connection.QueryAsync<SuperHero>("select * from SuperHeros");
+            IEnumerable<SuperHero> heros = await SelectAllHeros(connection);
+            return Ok(heros);
+        }
+        private static async Task<IEnumerable<SuperHero>> SelectAllHeros(SqlConnection connection)
+        {
+            return await connection.QueryAsync<SuperHero>("Select * from superheros");
+        }
+        [HttpGet("{heroID}")]
+        public async Task<ActionResult<SuperHero>> GetHero(int heroID)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var hero = await connection.QueryAsync<SuperHero>("select * from SuperHeros where ID=@ID", new { ID = heroID });
+            return Ok(hero);
+        }
+        [HttpDelete("{heroID}")]
+        public async Task<ActionResult<SuperHero>> DeleteHero(int heroID)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var hero = await connection.QueryAsync<SuperHero>("Delete from SuperHeros where ID=@ID", new { ID = heroID });
+            return Ok(await SelectAllHeros(connection));
+        }
+        [HttpPost]
+        public async Task<ActionResult<SuperHero>> CreateSupreHero(SuperHero hero)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("insert into SuperHeros(Name,FirstName,LastName,Place)values(@Name,@FirstName,@LastName,@Place)", hero);
+            return Ok(await SelectAllHeros(connection));
+        }
+        [HttpPut]
+        public async Task<ActionResult<SuperHero>> UpdateSupreHero(SuperHero hero)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("update SuperHeros set Name=@Name,FirstName=@FirstName,LastName=@LastName,Place=@place where ID=@ID", hero);
+            return Ok(await SelectAllHeros(connection));
+        }
+    }
+}
---
+++ DapperCrudOperation/SuperHero.cs
@@ -0,0 +1,11 @@
+﻿namespace DapperCrudOperation
+{
+    public class SuperHero
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }=string.Empty;
+        public string FirstName { get; set; }=string.Empty ;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;   
+    }
+}
